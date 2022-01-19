a7533785d07e8805094e1f006b4116a0bc0fdc4b
ElHombreMono <Pablo.decara@itgranada.com>
2022-01-19 12:58:45 +0100

    Probamos salto de linea con \n

---
+++ dia2.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+cout << "Imprimir por pantalla" << endl;
+cout << "Imprimir por pantalla\n";
+cout << "Comprobación de salto de linea" << endl;
+
+
+  return 0;
+}
