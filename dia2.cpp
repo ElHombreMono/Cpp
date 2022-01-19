bc1e9746e5a423378bae03297973591d35d7d61d
ElHombreMono <Pablo.decara@itgranada.com>
2022-01-19 13:00:32 +0100

    Sin acentos

--- dia2.cpp
+++ dia2.cpp
@@ -5,7 +5,7 @@ int main() {
 
 cout << "Imprimir por pantalla" << endl;
 cout << "Imprimir por pantalla\n";
-cout << "Comprobación de salto de linea" << endl;
+cout << "Comprobacion de salto de linea" << endl;
 
 
   return 0;
